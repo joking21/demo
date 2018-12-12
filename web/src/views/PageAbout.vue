34bd86ce422f507dac87790c381c4b40177dc017
deng.wenwen@datatom.com <123456@qq.com>
2018-12-12 17:48:42 +0800

    update

--- web/src/views/PageAbout.vue
+++ web/src/views/PageAbout.vue
@@ -1,4 +1,17 @@
-
 <template>
-    <div>about</div>
+  <div>
+      <div style="position:absolute; color: #ff0000">haaah</div>
+    <Register></Register>
+  </div>
 </template>
+<script>
+// 导入组件
+import Register from "../components/pageLogin/Bg.vue";
+// import
+export default {
+  components: {
+    Register,
+  }
+};
+</script>
+
