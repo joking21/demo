ee20baff6de979930b95c55a4fdbaf1e9b39e4b3
Joking <dengwenwen95@163.com>
2019-03-13 21:17:33 +0800

    update

--- server/routes/leaveAmessage.js
+++ server/routes/leaveAmessage.js
@@ -7,7 +7,7 @@ router.get('/list', function (req, res, next) {
 router.post('/add', function (req, res, next) {
     leaveAmessage.add(req, res, next);
 });
-router.get('/selectFromUser', function (req, res, next) {
+router.get('/selectByUser', function (req, res, next) {
     leaveAmessage.selectFromUser(req, res, next);
 })
 router.get('/delete', function (req, res, next) {
